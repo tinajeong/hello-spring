741fc8fa877e9db7062a620357e9702867995cdc
Tina <starandpoem143@gmail.com>
2021-03-13 01:24:16 +0900

    [스프링 웹 개발 기초] 개발 방식 3가지 정리

--- src/main/java/migu/spring/hellospring/controller/HelloController.java
+++ src/main/java/migu/spring/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package migu.spring.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -13,4 +15,35 @@ public class HelloController {
         return "hello"; // name of template file
     }
 
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam(value = "name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello hellApi(@RequestParam(value = "name")String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
