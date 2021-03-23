66e0430d92ff4636f06329fb133f86f134e861c2
Tina <starandpoem143@gmail.com>
2021-03-23 19:26:25 +0900

    [스프링 DB 접근 기술 1] - H2 데이터베이스 세팅 - jdbc 리포지토리 구현 - 통합 테스트 만들기

--- src/main/java/migu/spring/hellospring/SpringConfig.java
+++ src/main/java/migu/spring/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package migu.spring.hellospring;
 
+import migu.spring.hellospring.repository.JdbcMemberRepository;
 import migu.spring.hellospring.repository.MemberRepository;
 import migu.spring.hellospring.repository.MemoryMemberRepository;
 import migu.spring.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
